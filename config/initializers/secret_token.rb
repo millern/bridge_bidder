5727cd67cf3d876f7c80f5c6cfdcad2e7e9e8c16
= <njmiller6@gmail.com>
2013-02-15 20:36:06 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BridgeBidder::Application.config.secret_token = '219536c9bf750c8b539c21042fc588114684cdc749434db3f98ffedaf8fb3d1152903c17c4b8bf98e33de8e350a0fa24eaf927fd2deddb2a0d5270d46466f835'
